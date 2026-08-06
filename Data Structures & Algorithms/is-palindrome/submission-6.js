392f081aa469293cebfd5bf5a44bf01addaf58c1
João Matheus Oneta <101280415+joaooneta@users.noreply.github.com>
2026-08-06 15:52:19 -0300

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length-1;
+
+        while(left < right){
+            while(!this.alphaNum(s[left]) && left < right){
+                left++;
+            }
+            while(!this.alphaNum(s[right]) && right > left ){
+                right--;
+            }
+
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    alphaNum(c){
+        return ((c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || (c >= "0" && c <= "9"));
+    }
+}
