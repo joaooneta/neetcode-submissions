e86f8e160de951cd15358c10edbf84cacf20aa96
João Matheus Oneta <101280415+joaooneta@users.noreply.github.com>
2026-08-25 00:48:49 -0300

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0;
+        let charSet = new Set();
+        let longest = 0;
+
+        for(let right = 0; right < s.length; right++){
+            while(charSet.has(s[right])){
+                charSet.delete(s[left]);
+                left++;
+            }
+
+            charSet.add(s[right]);
+            let size = right - left + 1;
+            if(size > longest){
+                longest = size;
+            }
+        }
+
+        return longest;
+    }
+}
