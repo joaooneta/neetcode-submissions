0f317a3f42cfc4774c7542dcf27cc3023c199392
João Matheus Oneta <101280415+joaooneta@users.noreply.github.com>
2026-08-25 01:53:47 -0300

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const res = new Array(temperatures.length).fill(0);
+        const stack = [];
+        for(let i = 0; i < temperatures.length; i++){
+            while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length-1]]){
+                let prev = stack.pop();
+                res[prev] = i - prev;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
