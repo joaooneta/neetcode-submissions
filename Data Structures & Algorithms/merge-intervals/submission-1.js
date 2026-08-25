2a3787da41711768110a78d8f78b8f72fc17ccd9
João Matheus Oneta <101280415+joaooneta@users.noreply.github.com>
2026-08-25 01:04:16 -0300

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a,b) => a[0] - b[0]);
+        const res = [];
+        res.push(intervals[0]);
+
+        for(const interval of intervals){
+            const start = interval[0];
+            const end = interval[1];
+
+            const lastEnd = res[res.length-1][1];
+
+            if(start <= lastEnd){
+                res[res.length-1][1] = Math.max(lastEnd, end);
+            }else{
+                res.push([start, end]);
+            }
+        }
+
+        return res;
+    }
+}
