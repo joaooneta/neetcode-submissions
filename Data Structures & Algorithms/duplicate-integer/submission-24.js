8a189030f77a3654111acc4a3192db9adb4f2a9b
João Matheus Oneta <101280415+joaooneta@users.noreply.github.com>
2026-08-28 14:46:38 -0300

    Add: duplicate-integer - submission-24

---
+++ Data Structures & Algorithms/duplicate-integer/submission-24.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const numsSet = new Set();
+        for(const n of nums){
+            if(numsSet.has(n)) return true;
+            numsSet.add(n);
+        }
+        return false;
+    }
+}
